a2e3ee6bd22c5a78b3b2124eda97d21389019e05
Lily <soranorio911128@gmail.com>
2021-03-16 22:36:47 +0800

    - 任务系统数据结构v2

--- game-dao/src/main/java/com/nekonade/dao/db/entity/data/task/BasicTask.java
+++ game-dao/src/main/java/com/nekonade/dao/db/entity/data/task/BasicTask.java
@@ -1,10 +1,22 @@
 package com.nekonade.dao.db.entity.data.task;
 
+import com.fasterxml.jackson.annotation.JsonSubTypes;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import lombok.Getter;
 import lombok.Setter;
 
+
+
+
 @Getter
 @Setter
+@JsonTypeInfo(
+        use = JsonTypeInfo.Id.CLASS,
+        include = JsonTypeInfo.As.PROPERTY)
+/*@JsonSubTypes({
+        @JsonSubTypes.Type(value = DayFirstLoginTask.class, name = "DayFirstLoginTask"),
+        @JsonSubTypes.Type(value = ConsumeGoldTask.class, name = "ConsumeGoldTask")
+})*/
 public abstract class BasicTask {
 
     public abstract boolean finishCheck();
