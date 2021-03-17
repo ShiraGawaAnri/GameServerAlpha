eabadb870055aca11f8dd5863f2b30b370ae5d3b
yukicute <298539237@qq.com>
2021-03-17 18:39:28 +0800

    - fix

--- game-dao/src/main/java/com/nekonade/dao/daos/db/TasksDbDao.java
+++ game-dao/src/main/java/com/nekonade/dao/daos/db/TasksDbDao.java
@@ -1,6 +1,7 @@
 package com.nekonade.dao.daos.db;
 
 import com.nekonade.common.redis.EnumRedisKey;
+import com.nekonade.common.utils.JacksonUtils;
 import com.nekonade.dao.daos.AbstractDao;
 import com.nekonade.dao.db.entity.data.task.TasksDB;
 import com.nekonade.dao.db.repository.TasksDbRepository;
@@ -8,6 +9,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.annotation.CacheConfig;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @CacheConfig(cacheManager = "caffeineCacheManager")
 @Service
@@ -29,5 +33,19 @@ public class TasksDbDao extends AbstractDao<TasksDB, String> {
     @Override
     protected Class<TasksDB> getEntityClass() {
         return TasksDB.class;
+    }
+
+    @Override
+    public Map<String, TasksDB> findAllInMap() {
+        String key = this.getRedisKey().getKey();
+        Map<Object, Object> entries = redisTemplate.opsForHash().entries(key);
+        Map<String, TasksDB> map = new HashMap<>();
+        entries.forEach((key1, value) -> {
+            String newKey = key1.toString();
+            String newJson = value.toString();
+            TasksDB entity = JacksonUtils.parseObjectV2(newJson,TasksDB.class);
+            map.put(newKey, entity);
+        });
+        return map;
     }
 }
--- game-neko/src/main/java/com/nekonade/neko/logic/task/TaskEnumCollections.java
+++ game-neko/src/main/java/com/nekonade/neko/logic/task/TaskEnumCollections.java
@@ -9,22 +9,20 @@ public class TaskEnumCollections {
 
     @Getter
     public enum EnumTaskType {
-        ConsumeGold(1, new AccumulationTaskProgress(), "消耗x金币",ConsumeGoldTask.class),
-        ConsumeDiamond(2, new AccumulationTaskProgress(), "消耗x钻石",null),
-        PassBlockPoint(3, null, "通关某个关卡",null),
-        PassBlockPointTimes(4, null, "通关某个关卡多少次",null),
-        DayFirstLogin(5, new AccumulationTaskProgress(), "每日首次登录", DayFirstLoginTask.class),
+        ConsumeGold(1, new AccumulationTaskProgress(), "消耗x金币"),
+        ConsumeDiamond(2, new AccumulationTaskProgress(), "消耗x钻石"),
+        PassBlockPoint(3, null, "通关某个关卡"),
+        PassBlockPointTimes(4, null, "通关某个关卡多少次"),
+        DayFirstLogin(5, new AccumulationTaskProgress(), "每日首次登录"),
         ;
         private final int type;
         private final ITaskProgress taskProgress;
         private final String desc;
-        private final Class<? extends BasicTask> clazz;
 
-        EnumTaskType(int type, ITaskProgress taskProgress, String desc, Class<? extends BasicTask> clazz) {
+        EnumTaskType(int type, ITaskProgress taskProgress, String desc) {
             this.type = type;
             this.taskProgress = taskProgress;
             this.desc = desc;
-            this.clazz = clazz;
         }
     }
 }
